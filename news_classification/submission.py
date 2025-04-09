b75fdfa245ecef6759ab56f048c75cb9642ab724
pandea23 <pandea23@s4090a.cas.mcmaster.ca>
2025-04-09 17:29:57 -0400

    Added submissions.py to submit on codabench

---
+++ news_classification/submission.py
@@ -0,0 +1,5 @@
+submissionType = 1  # This must be included and is used to tell our code that this is an integer array submission
+
+def predictionResults():
+    # returning the predictions as hard-coded values which I copied from the csv
+    return [0, 2, 2, 3, 0, 0, 4, 2, 0, 3, 3, 4, 0, 0, 2, 3, 0, 4, 2, 0, 2, 3, 0, 0, 0, 3, 0, 0, 0, 7, 3, 3, 0, 0, 2, 2, 3, 3, 0, 3, 3, 2, 0, 4, 0, 3, 3, 0, 0, 2, 0, 0, 2, 3, 3, 0, 0, 3, 0, 2, 2, 2, 2, 2, 2, 0, 0, 3, 0, 3, 3, 2, 0, 3, 7, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 3, 3, 0, 0, 0, 0, 0, 7, 0, 4, 7, 6, 2, 0, 4, 0, 0, 0, 0, 4, 2, 0, 3, 3, 3, 0, 0, 2, 2, 7, 0, 2, 7, 0, 2, 0]
